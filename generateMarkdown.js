7cdafcea0bfd575f00f015d8a592e1cc46f5cebe
Daniel Lawrence <dan.lawrence0810@gmail.com>
2022-02-10 11:01:47 +1030

    renderLicenseSection function written

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,6 +1,7 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
+  let response;
   switch (license) {
     case 'MIT':
       response = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
@@ -16,18 +17,63 @@ function renderLicenseBadge(license) {
       break;
     default:
       response = '';
+      break;
   }
   return response;
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// function renderLicenseLink(license) {
+//   let licenceLink;
+//   switch (license) {
+//     case 'MIT':
+//       licenceLink = ;
+//       break;
+//     case 'ISC':
+//       licenceLink = ;
+//       break;
+//     case 'GNU General Public License v3':
+//       licenceLink = ;
+//       break;
+//     case 'Apache 2.0':
+//       licenceLink = ;
+//       break;
+//     default:
+//       licenceLink = '';
+//       break;
+//   }
+// }
 //
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  let licenseSection;
+  switch (license) {
+    case 'MIT':
+      licenseSection = '##License\
+      This project is MIT licensed, please see the badge at the top of the page';
+      break;
+    case 'ISC':
+      licenseSection = '##License\
+      This project is ISC licensed, please see the badge at the top of the page';
+      break;
+    case 'GNU General Public License v3':
+      licenseSection = '##License\
+      This project is GPL v3 licensed, please see the badge at the top of the page';
+      break;
+    case 'Apache 2.0':
+      licenseSection = '##License\
+      This project is Apache 2.0 licensed, please see the badge at the top of the page';
+      break;
+    default:
+      licenseSection = '';
+      break;
+  }
+  return licenseSection
+     
+}
 
 // TODO: Create a function to generate markdown for README
 // installation section - have npm i (project name) as a default option if enter pressed
@@ -54,7 +100,6 @@ function generateMarkdown(data) {
   ## Usage
   ${data.usage}
 
-  ##License
   ${renderLicenseSection(data.license)}
 
   ## Contributing
