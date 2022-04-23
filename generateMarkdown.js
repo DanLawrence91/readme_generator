6afbc009876d1ba327baf6d9b3cd176bf6957bba
Daniel Lawrence <dan.lawrence0810@gmail.com>
2022-04-23 12:19:54 +0930

    updates to readme file generation

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -3,23 +3,23 @@
 function renderLicenseBadge(license) {
   let response;
   switch (license) {
-    case 'MIT':
-      response = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+    case "MIT":
+      response = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
       break;
-    case 'ISC':
-      response = '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)';
+    case "ISC":
+      response = "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)";
       break;
-    case 'GNU General Public License v3':
-      response = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+    case "GNU General Public License v3":
+      response = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
       break;
-    case 'Apache 2.0':
-      response = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+    case "Apache 2.0":
+      response = "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
       break;
-    case 'Other':
-      response = 'Please add a license badge here. The following link has a number of common license badges for use: [https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba](https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba)';
+    case "Other":
+      response = "Please add a license badge here. The following link has a number of common license badges for use: [https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba](https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba)";
       break;
-    case 'No license':
-      response = '';
+    case "No license":
+      response = "";
       break;
   }
   return response;
@@ -54,27 +54,26 @@ function renderLicenseBadge(license) {
 function renderLicenseSection(license) {
   let licenseSection;
   switch (license) {
-    case 'MIT':
-      licenseSection = '## License\n\tThis project is MIT licensed, please see the badge at the top of the page';
+    case "MIT":
+      licenseSection = "## License\n\tThis project is MIT licensed, please see the badge at the top of the page";
       break;
-    case 'ISC':
-      licenseSection = '## License\n\tThis project is ISC licensed, please see the badge at the top of the page';
+    case "ISC":
+      licenseSection = "## License\n\tThis project is ISC licensed, please see the badge at the top of the page";
       break;
-    case 'GNU General Public License v3':
-      licenseSection = '## License\n\tThis project is GPL v3 licensed, please see the badge at the top of the page';
+    case "GNU General Public License v3":
+      licenseSection = "## License\n\tThis project is GPL v3 licensed, please see the badge at the top of the page";
       break;
-    case 'Apache 2.0':
-      licenseSection = '## License\n\tThis project is Apache 2.0 licensed, please see the badge at the top of the page';
+    case "Apache 2.0":
+      licenseSection = "## License\n\tThis project is Apache 2.0 licensed, please see the badge at the top of the page";
       break;
-    case 'Other':
-      licenseSection = '## License\n\tPlease include details of selected license here';
+    case "Other":
+      licenseSection = "## License\n\tPlease include details of selected license here";
       break;
-    case 'No license':
-      licenseSection = '';
+    case "No license":
+      licenseSection = "";
       break;
   }
-  return licenseSection
-     
+  return licenseSection;
 }
 
 // TODO: Create a function to generate markdown for README
@@ -93,9 +92,12 @@ function generateMarkdown(data) {
   - [Usage](#usage)
   - [License](#license)
   - [Contributing](#contributing)
-  - [Tests](#tests)
+  - [Deployed site](#links)
   - [Questions](#questions)
 
+  ## Technology used
+  ${data.technology}
+  
   ## Installation
   ${data.installation}
 
@@ -107,8 +109,8 @@ function generateMarkdown(data) {
   ## Contributing
   ${data.contributing}
 
-  ## Tests
-  ${data.tests}
+  ## Deployed Site
+  ${data.links}
 
   ## Questions
   If there are any questions regarding this project please contact me through my email - ${data.email}
@@ -118,4 +120,4 @@ function generateMarkdown(data) {
 
 module.exports = {
   generateMarkdown,
-}
+};
--- index.js
+++ index.js
@@ -1,73 +1,77 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer');
-const fs = require('fs');
-const readme = require('./generateMarkdown.js')
+const inquirer = require("inquirer");
+const fs = require("fs");
+const readme = require("./generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
 const questions = [
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What is the title of your project?'
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Please give a description of your project',
-    },
-    {
-        type: 'input',
-        name: 'installation',
-        message: 'Please explain how to install your project',
-        default: 'npm i (project name)'
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'Please explain how to use the project',
-    },
-    {
-        type: 'list',
-        name: 'license',
-        message: 'Please choose a license. Please select no license if there is no license and this section will not generate',
-        choices: ['MIT', 'ISC', 'GNU General Public License v3', 'Apache 2.0', 'Other', 'No license'],
-    },
-    {
-        type: 'input',
-        name: 'contributing',
-        message: 'Please give guidelines on how to contribute to this project',
-        default: 'Contributions are welcome via pull requests. If anything major is to be proposed, please open an issue first to discuss what you would like to change.'
-    },
-    {
-        type: 'input',
-        name: 'tests',
-        message: 'Please explain how to run any necessary tests',
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'Please provide your email address',
-    },
-    {
-        type: 'input',
-        name: 'github',
-        message: 'Pleaase provide your GitHub username',
-    }
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please give a description of your project",
+  },
+  {
+    type: "input",
+    name: "technology",
+    message: "List tech used",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Please explain how to install and deploy your project",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Please explain how to use the project",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Please choose a license. Please select no license if there is no license and this section will not generate",
+    choices: ["MIT", "ISC", "GNU General Public License v3", "Apache 2.0", "Other", "No license"],
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "Please give guidelines on how to contribute to this project",
+    default: "Contributions are welcome via pull requests. If anything major is to be proposed, please open an issue first to discuss what you would like to change.",
+  },
+  {
+    type: "input",
+    name: "links",
+    message: "Provide Links",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Please provide your email address",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "Pleaase provide your GitHub username",
+  },
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-
-    return fs.writeFileSync(fileName, data)
+  return fs.writeFileSync(fileName, data);
 }
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer.prompt(questions)
-        .then ((data) => writeToFile('README.md', readme.generateMarkdown(data)))
-        .then (() => console.log('README creation successful'))
-        .catch ((err) => console.error(err))
+  inquirer
+    .prompt(questions)
+    .then((data) => writeToFile("newREADME.md", readme.generateMarkdown(data)))
+    .then(() => console.log("README creation successful"))
+    .catch((err) => console.error(err));
 }
 
 // Function call to initialize app
-init(); 
+init();
